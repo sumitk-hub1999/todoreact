f798b401e38df34c272c28a19c3747a69eb2ad7c
sumitk-hub1999 <skatyal1999@gmail.com>
2023-03-20 07:24:15 +0530

    making input work

--- todolistst/src/App.jsx
+++ todolistst/src/App.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
   // react fragment syntactic sugar
+  const [inputEntry, setInput] = useState("");
+
+  //defining a function to be used as event
+  const clickEvent = (e) => {
+    setInput(e.target.value); //creating an event, now what we write in input is visible as li
+  };
+  //defining addItems
+  const addItems = () => {};
+  //using usestate hook
   return (
     <>
+      {/* //we need state and to get state we will use hooks */}
+
       <div className="main">
         <div className="container">
           <br />
           <h1>TODO LIST</h1>
           <br />
-          <input type="text" placeholder="Add items" />
-          <button>Add task +</button>
+          <input type="text" placeholder="Add items" onChange={clickEvent} />
+          <button onClick={addItems}>Add task +</button>
 
           <ol>
-            <li>buy a car</li>
+            <li>{inputEntry}</li>
           </ol>
         </div>
       </div>
